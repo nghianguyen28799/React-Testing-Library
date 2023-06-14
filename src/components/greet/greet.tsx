42268221a434978159e9c7dd3350de7fbc3a50b9
Nghia <nghianguyen28799@gmail.com>
2023-06-14 22:34:20 +0700

    first commit

---
+++ src/components/greet/greet.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { GreetProps } from './greet.types'
+
+export const Greet = ({ name }: GreetProps ) => {
+  return (
+    // <div>Hello {name ? name : "Guest"}</div> // branch 50%
+    <div>Hello {name}</div> // branch 50%
+  )
+}
